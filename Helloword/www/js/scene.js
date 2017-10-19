01c5683961224708e90cb34905a1fe2477c32b98
luisfayre <15551452@lc_047.itchii.lab>
2017-10-19 09:56:52 -0600

    render
    render

--- Helloword/www/js/scene.js
+++ Helloword/www/js/scene.js
@@ -1,5 +1,21 @@
 (function () {
 	var scene = new THREE.Scene();
-	let camera = new THREE.PerspectiveCamera();
+	const aspectRatio = window.innerWidth/window.innerHeight;
+	var camera = new THREE.PerspectiveCamera(75,aspectRatio,0.1,100);
+
+	var renderer = new THREE.WebGLRenderer();
+	renderer.setSize(window.innerWidth,window.innerHeight);
+
+	document.body.appendChild(renderer.domElement);
+
+	//renderer.render(scene, camera);
+
+	function loop(){
+		requestAnimationFrame(loop);
+		console.log("new frame");
+		renderer.render(scene, camera);
+	}
+
+	loop();
 	
 })();
